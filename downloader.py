c3a63c947a66fe190ab6b7d2fce2d37751a61e4b
johnwtian <66046129+johnwtian@users.noreply.github.com>
2022-10-09 17:33:19 -0700

    Adding CLI functionality with argparse

--- downloader.py
+++ downloader.py
@@ -1,8 +1,12 @@
 
 #Credit https://www.youtube.com/watch?v=vEQ8CXFWLZU&t=278s
+from pickletools import string1
+import string
 from pytube import YouTube
 from sys import argv
 from pathlib import Path
+import argparse
+import os
 
 class YouTubeDownloader(object):
 
@@ -16,30 +20,82 @@ class YouTubeDownloader(object):
         print("Title: ", self.yt.title)
         print("View: ", self.yt.views)
 
-    def download(self):
+    def download(self, output= None):
         yd = self.yt.streams.get_highest_resolution()
-        yd.download(self.downloads_path)
+        if(output):
+            yd.download(output)
+        else:
+            yd.download(self.downloads_path)
 
-def main(link):
+def return_urls(filepath):
+    urls = []
+    
+    with open(filepath) as f:
+        path = f.readline()
+        while(path is not None):
+            urls.append(path)
+            path = f.readline()
+    return urls
+
+def progress_func(*args):
+    print(args)
+
+def main(link, output= None):
+    #try:
     downloader = YouTubeDownloader(link)
-    print("Download Started!")
-    downloader.download()
+    # downloader.register_on_progress_callback(progress_func)
+    print(f"/n/'{downloader.yt.title}/' Download Started!")
+    downloader.download(output)
     print("Done!")
+    #except:
+    #    print(f"Could not find link {link}!")
 
 if __name__ == "__main__":
 
-    print("Welcome to YouTube downloader!")
+    parser = argparse.ArgumentParser(description= "Downloads YouTube Videos")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-l", "--link", help= "Provide link to Youtube URL")
+    group.add_argument("-f", "--filepath", help= "Provide filepath to input Youtube URLs")
+    parser.add_argument("-o", "-output_path", help="Optional - output path location")
+    parser.add_argument("-p", "--prompt", choices=[True, False], help="Run using prompts")
+    parser.add_argument("-r", "--resolution", choices=["high", "low"], default="low", help="Specify high or low resolution")
 
-    format = input("Select single (L)ink or (F)ile download: ")
 
-    if (format.lower() == "l"):
-        # Single link
-        link = input("Link: ")
-        main(link)
-    elif (format.lower() == "f"):
-        # File
-        pass
+    args = parser.parse_args()
+
+    # print(args)
+
+    if((args.prompt == True)):
+        #Process using prompt method
+        print("Welcome to YouTube downloader!")
+        format = input("Select single (L)ink or (F)ile download: ")
+
+        if (format.lower() == "l"):
+            # Single link
+            link = input("Link: ")
+            main(link)
+        elif (format.lower() == "f"):
+            # File
+            print(f"Current filepath: {os.getcwd()}")
+            path = input("Path: ")
+            links = return_urls(path)
+            for link in links:
+                main(link)
+        else:
+            print("Invalid option!")
+    elif((args.link == None) & (args.filepath == None)):
+        print("Need to provide link or filepath!")
     else:
-        print("Invalid option!")
+        #Process filepath or link
+        print("Welcome to YouTube downloader!")
+        if(args.link is not None):
+            #Process link
+            main(args.link)
+        else:
+            #Process filepath
+            links = return_urls(args.filepath)
+            for link in links:
+                main(link)
 
-    
+
+
